eef361d0b3953130b96021bb3cf68930eec2d54c
Jakub Gorzelak <jakub.gorzelak@gmail.com>
2022-12-03 17:23:39 +0100

    day 2 part 2

--- 2.exs
+++ 2.exs
@@ -1,12 +1,16 @@
+# part 1
 player_1_shape_by_letter = %{"A" => :rock, "B" => :paper, "C" => :scissors}
 player_2_shape_by_letter = %{"X" => :rock, "Y" => :paper, "Z" => :scissors}
 
 points_by_round_outcome = %{lose: 0, draw: 3, win: 6}
 points_by_shape = %{rock: 1, paper: 2, scissors: 3}
 
-File.read!("2_input.txt")
-|> String.trim
-|> String.split("\n")
+rounds = 
+  File.read!("2_input.txt")
+  |> String.trim
+  |> String.split("\n")
+
+rounds
 |> Enum.reduce(0, fn shapes, score ->
   [player_1_letter, player_2_letter] = shapes |> String.split(" ")
 
@@ -30,3 +34,30 @@ File.read!("2_input.txt")
 end)
 |> IO.inspect
 
+# part 2
+
+outcome_by_letter = %{"X" => :lose, "Y" => :draw, "Z" => :win}
+
+rounds
+|> Enum.reduce(0, fn round_letters, score ->
+  [player_1_letter, outcome_letter] = round_letters |> String.split(" ")
+
+  player_1_shape = player_1_shape_by_letter |> Map.get(player_1_letter)
+  player_2_outcome = outcome_by_letter |> Map.get(outcome_letter)
+
+  player_2_shape =
+    case {player_1_shape, player_2_outcome} do
+      {:rock, :win} -> :paper
+      {:rock, :lose} -> :scissors
+      {:paper, :win} -> :scissors
+      {:paper, :lose} -> :rock
+      {:scissors, :win} -> :rock
+      {:scissors, :lose} -> :paper
+      _ -> player_1_shape
+    end
+
+    score +
+      Map.get(points_by_round_outcome, player_2_outcome) +
+      Map.get(points_by_shape, player_2_shape)
+end)
+|> IO.inspect
