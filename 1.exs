1fa692838fe5ff4a383e1065ab669f44a55a09d1
Jakub Gorzelak <jakub.gorzelak@gmail.com>
2022-12-03 16:34:06 +0100

    part two for day 1

--- 1.exs
+++ 1.exs
@@ -1,14 +1,27 @@
-File.read!("1_input.txt")
-|> String.split("\n\n")
-|> Enum.map(fn food ->
-  food
-  |> String.trim
-  |> String.split("\n")
-  |> Enum.reduce(0, fn cals, memo -> memo + String.to_integer(cals) end)
-end)
-|> Enum.with_index
-|> Enum.reduce({0, 0}, fn {cals, index}, {max_index, max_cals} ->
-  if cals > max_cals,
-    do: {index, cals},
-    else: {max_index, max_cals}
-end)
+# part 1
+
+sorted_by_cals =
+  File.read!("1_input.txt")
+  |> String.split("\n\n")
+  |> Enum.map(fn food ->
+    food
+    |> String.trim
+    |> String.split("\n")
+    |> Enum.reduce(0, fn cals, memo -> memo + String.to_integer(cals) end)
+  end)
+  |> Enum.with_index
+  |> Enum.sort_by(fn {cals, index} -> cals end)
+  |> Enum.reverse
+
+sorted_by_cals
+|> Enum.at(0)
+|> elem(0)
+|> IO.inspect
+
+# part 2
+
+sorted_by_cals
+|> Enum.take(3)
+|> Enum.reduce(0, fn {cals, _}, memo -> memo + cals end)
+|> IO.inspect
+
