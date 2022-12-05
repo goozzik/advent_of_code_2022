6a1cc13267766c4570dd0ad941a70f96985dd789
Jakub Gorzelak <jakub.gorzelak@gmail.com>
2022-12-05 19:15:39 +0100

    day 4 part 1

---
+++ 4.exs
@@ -0,0 +1,21 @@
+File.read!("4_input.txt")
+|> String.trim
+|> String.split("\n")
+|> Enum.reduce(0, fn sections, memo ->
+  [elv_1_section_start, elv_1_section_end, elv_2_section_start, elv_2_section_end] =
+    sections
+    |> String.split(",")
+    |> Enum.flat_map(fn elv_section_range ->
+      elv_section_range
+      |> String.split("-")
+      |> Enum.map(&String.to_integer/1)
+    end)
+
+  if (elv_1_section_start <= elv_2_section_start && elv_1_section_end >= elv_2_section_end) ||
+    (elv_2_section_start <= elv_1_section_start && elv_2_section_end >= elv_1_section_end) do
+    memo + 1
+  else
+    memo
+  end
+end)
+|> IO.inspect
