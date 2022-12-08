e649a76653a897acea51af1b7adb17cda26acbc3
Jakub Gorzelak <jakub.gorzelak@gmail.com>
2022-12-08 19:42:29 +0100

    day 5 part 1

---
+++ 5.exs
@@ -0,0 +1,49 @@
+input_file_name = "5_input.txt"
+#input_file_name = "5_example.txt"
+
+[crate_matrix, rearragment_steps] = 
+  File.read!(input_file_name)
+  |> String.split("\n\n")
+
+crate_matrix = 
+  crate_matrix
+  |> String.split("\n")
+  |> Enum.drop(-1)
+  |> Enum.reduce(%{}, fn row, memo -> 
+    row
+    |> Stream.unfold(&String.split_at(&1, 4))
+    |> Enum.take_while(&(&1 != ""))
+    |> Enum.with_index(1)
+    |> Enum.reduce(memo, fn {crate, stack_index}, memo ->
+      case crate |> String.trim do
+        "" -> memo
+        crate ->
+          case memo |> Map.get(stack_index) do
+            nil -> memo |> Map.put(stack_index, [crate])
+            stack -> memo |> Map.put(stack_index, [crate | stack])
+          end
+      end
+    end)
+  end)
+
+rearragment_steps
+|> String.split("\n")
+|> Enum.reverse
+|> Enum.drop(1)
+|> Enum.reverse
+|> Enum.reduce(crate_matrix, fn rearragment_step, memo ->
+  [_, count, _, from, _, to] = rearragment_step |> String.split("\n") |> List.first |> String.split(" ")
+  move = count |> String.to_integer
+  from = from |> String.to_integer
+  to = to |> String.to_integer
+
+  {crates_left, crates_to_move} = memo[from] |> Enum.split(-(move))
+  memo
+  |> Map.put(from, crates_left)
+  |> Map.put(to, Map.get(memo, to) ++ Enum.reverse(crates_to_move))
+end)
+|> Map.values
+|> Enum.map(& List.last(&1))
+|> Enum.map(& String.at(&1, 1))
+|> Enum.join
+|> IO.inspect
