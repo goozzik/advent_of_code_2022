5a4e1f40ee648ff9ee4fc6bbc189ac27c9441396
Jakub Gorzelak <jakub.gorzelak@gmail.com>
2022-12-09 21:10:11 +0100

    part 2 day 6

--- 6.exs
+++ 6.exs
@@ -1,26 +1,29 @@
 chars =
   "6_input.txt"
   |> File.read!
-  # "bvwbjplbgvbhsrlpgdmjqwftvncz"
   |> String.graphemes
-# require IEx; IEx.pry()
 
-first_index = 
-  chars
-  |> Enum.with_index
-  |> Enum.find(fn {_char, index} ->
-    len =
+find_position =
+  fn chars, uniq_chars_lenght ->
+    first_index = 
       chars
-      |> Enum.slice(index, 4)
-      |> IO.inspect
-      |> Enum.uniq
-      |> length
+      |> Enum.with_index
+      |> Enum.find(fn {_char, index} ->
+        len =
+          chars
+          |> Enum.slice(index, uniq_chars_lenght)
+          |> Enum.uniq
+          |> length
 
-    len == 4
-  end)
-  |> elem(1)
+        len == uniq_chars_lenght
+      end)
+      |> elem(1)
+    first_index + uniq_chars_lenght
+  end
 
 # part 1
-IO.inspect(first_index + 4)
+find_position.(chars, 4) |> IO.inspect()
+# part 2
+find_position.(chars, 14) |> IO.inspect()
 
 
